28c15af7f9a2f27cd6f491b4de75dd81c39771cd
MuratBeshimow <muratbeshimow1@gmail.com>
2023-09-03 21:24:47 +0300

    flask3

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
-import flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/home')
 def hello():
-    return render_template("index.html")
+    return render_template("base.html")
 
 
 @app.route('/view')
